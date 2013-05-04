8c7cd36016b7066dded6295045b7126f0dce9de4
Vincent Barthélémy <vincent.barthelemy.perso@gmail.com>
2013-05-04 20:25:06 +0200

    Create a dummy skeleton for the js part of the platform

--- js/moka-platform.js
+++ js/moka-platform.js
@@ -2,3 +2,32 @@
 *    Dependencies
 *       jQuery
 */
+
+var Moka = Moka || {};
+
+Moka.plateform = (function(){
+    "use strict";
+    
+    //private properties & methods
+    var pong =  "pong";
+    
+    //public API -- constructor
+    var MokaPlatform = function(){
+        //attributes  
+    }
+    
+    //public API -- methods
+    MokaPlatform.prototype = {
+        
+        ping : function(){
+            console.log(pong);
+        },
+    
+    };
+    
+    return MokaPlatform;    
+})();
+
+
+
+    
